e7e5b2f51394eb9df6b32f478fcd15c7b2c6b045
Vesa M S Piilola <vpiilola@z1-1.cs.helsinki.fi>
2014-01-16 16:52:55 +0200

    Netbeans -projektin aloitus

---
+++ Laivanupotus/src/main/java/laivanupotus/laivanupotus/App.java
@@ -0,0 +1,13 @@
+package laivanupotus.laivanupotus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
